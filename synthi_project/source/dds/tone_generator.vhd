15e4170ab4741c8232f084ac029ed5483bc3b00d
And-Dm <127297557+And-Dm@users.noreply.github.com>
2023-04-12 10:30:28 +0200

    MS3 1 dds

--- synthi_project/source/dds/tone_generator.vhd
+++ synthi_project/source/dds/tone_generator.vhd
@@ -6,7 +6,7 @@
 -- Author     :   <andri@DESKTOP-03G9R51>
 -- Company    : 
 -- Created    : 2023-04-03
--- Last update: 2023-04-07
+-- Last update: 2023-04-11
 -- Platform   : 
 -- Standard   : VHDL'08
 -------------------------------------------------------------------------------
@@ -30,17 +30,16 @@ use work.tone_gen_pkg.all;
 
 entity tone_generator is
 
-  generic (width : positive := 16);
 
   port (
     tone_on_i   : IN std_logic;
-    note_l_i    : IN std_logic_vector(6 downto 0);
+    note_i      : IN std_logic_vector(6 downto 0);
     step_i      : IN std_logic;
-    velocity_i  : IN std_logic;
+    velocity_i  : IN std_logic_vector(6 downto 0);
     clk_6m      : IN std_logic;
     rst_n       : IN std_logic;
-    dds_l_o     : OUT std_logic_vector(width-1 downto 0);
-    dds_r_o     : OUT std_logic_vector(width-1 downto 0)
+    dds_l_o     : OUT std_logic_vector(N_AUDIO-1 downto 0);
+    dds_r_o     : OUT std_logic_vector(N_AUDIO-1 downto 0)
 
     );
 
@@ -58,13 +57,37 @@ architecture str of tone_generator is
   -- Component declarations
   -----------------------------------------------------------------------------
 
+  component dds is
+    port (
+      clk_6m   : in  std_logic;
+      reset_n  : in  std_logic;
+      phi_incr : in  std_logic_vector(N_CUM-1 downto 0);
+      step     : in  std_logic;
+      tone_on  : in  std_logic;
+      attenu_i : in  std_logic_vector(2 downto 0);
+      dds_o    : out std_logic_vector(N_AUDIO-1 downto 0));
+  end component dds;
+
 
 	 
-
+signal dds_o  : std_logic_vector(N_AUDIO-1 downto 0);
 
 
 begin
 
+  -- instance "dds_1"
+  dds_1: dds
+    port map (
+      clk_6m   => clk_6m,
+      reset_n  => rst_n,
+      phi_incr => LUT_midi2dds(to_integer(unsigned(note_i))),
+      step     => step_i,
+      tone_on  => tone_on_i,
+      attenu_i => velocity_i(6 downto 4),
+      dds_o    => dds_o);
+
+      dds_l_o <= dds_o;
+      dds_r_o <= dds_o;
 
 
 end architecture str;
